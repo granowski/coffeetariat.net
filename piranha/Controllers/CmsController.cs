c373c8dcc8e61ebc36c3021fc71d34e12601d105
Derrick Granowski <granowski@coffeetariat.net>
2019-07-20 21:42:45 -0600

    initial check-in of cms blog framework 'Piranha CMS' for my use; configured to use Postgres

---
+++ piranha/Controllers/CmsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Piranha;
+using blog.Models;
+
+namespace blog.Controllers
+{
+    public class CmsController : Controller
+    {
+        private readonly IApi _api;
+
+        public CmsController(IApi api)
+        {
+            _api = api;
+        }
+
+        [Route("archive")]
+        public async Task<IActionResult> Archive(Guid id, int? year = null, int? month = null, int? page = null,
+            Guid? category = null, Guid? tag = null)
+        {
+            var model = await _api.Pages.GetByIdAsync<BlogArchive>(id);
+            model.Archive = await _api.Archives.GetByIdAsync(id, page, category, tag, year, month);
+
+            return View(model);
+        }
+
+        [Route("page")]
+        public async Task<IActionResult> Page(Guid id)
+        {
+            var model = await _api.Pages.GetByIdAsync<StandardPage>(id);
+
+            return View(model);
+        }
+
+        [Route("pagewide")]
+        public async Task<IActionResult> PageWide(Guid id)
+        {
+            var model = await _api.Pages.GetByIdAsync<StandardPage>(id);
+
+            return View(model);
+        }
+
+        [Route("post")]
+        public async Task<IActionResult> Post(Guid id)
+        {
+            var model = await _api.Posts.GetByIdAsync<BlogPost>(id);
+
+            return View(model);
+        }
+    }
+}
---
+++ piranha/Startup.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Piranha;
+using Piranha.AspNetCore.Identity.PostgreSQL;
+
+namespace blog
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(config =>
+            {
+                config.ModelBinderProviders.Insert(0,
+                    new Piranha.Manager.Binders.AbstractModelBinderProvider());
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddPiranha();
+            services.AddPiranhaApplication();
+            services.AddPiranhaFileStorage();
+            services.AddPiranhaImageSharp();
+            services.AddPiranhaManager();
+            services.AddPiranhaMemoryCache();
+
+            services.AddPiranhaEF(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("piranha"));
+            });
+            services.AddPiranhaIdentityWithSeed<IdentityPostgreSQLDb>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("piranha"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApi api)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            App.Init(api);
+
+            App.CacheLevel = Piranha.Cache.CacheLevel.Basic;
+
+            var pageTypeBuilder = new Piranha.AttributeBuilder.PageTypeBuilder(api)
+                .AddType(typeof(Models.BlogArchive))
+                .AddType(typeof(Models.StandardPage));
+            pageTypeBuilder.Build()
+                .DeleteOrphans();
+            var postTypeBuilder = new Piranha.AttributeBuilder.PostTypeBuilder(api)
+                .AddType(typeof(Models.BlogPost));
+            postTypeBuilder.Build()
+                .DeleteOrphans();
+            var siteTypeBuilder = new Piranha.AttributeBuilder.SiteTypeBuilder(api)
+                .AddType(typeof(Models.BlogSite));
+            siteTypeBuilder.Build()
+                .DeleteOrphans();
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UsePiranha();
+            app.UsePiranhaManager();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller}/{action}/{id?}",
+                    defaults: new {controller = "Home", action = "Index"});
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=home}/{action=index}/{id?}");
+            });
+        }
+    }
+}
