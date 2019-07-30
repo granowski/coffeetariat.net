013a18965b70d699b920a9ca1930ab10f26cf2c6
Derrick Granowski <granowski@coffeetariat.net>
2019-07-29 21:30:29 -0600

    wip; drop in the web example for tweaking

--- w3/ClientApp/src/components/Marquee.tsx
+++ w3/ClientApp/src/components/Marquee.tsx
@@ -1,33 +1,15 @@
 import React, { FunctionComponent } from 'react';
 
-import 'Marquee.css';
+import './Marquee.css';
 
-let marqueeStyle = {
-  overflow: 'hidden',
-  position: 'relative',
-  height: '25px',
-  width: '420px',
-};
-
-let marqueeDiv = {
-  position: 'absolute',
-  overflow: 'hidden',
-  display: 'block',
-  width: '200%',
-  height: '30px',
-  animation: 'marquee 5s linear infinite'
-};
-
-let marqueeSpan = {
-  float: 'left',
-  width: '50%'
-};
-
-export let Marquee: FunctionComponent<{}> = (props) => {
+export let Marquee: FunctionComponent<{text: string}> = (props) => {
   let text = props.text || '';
   return (
-    <div >
-      {text}
+    <div className="marquee">
+      <div>
+        <span>{text}</span>
+        <span>{text}</span>
+      </div>
     </div>
   )
 };
--- w3/ClientApp/src/components/NavList.js
+++ w3/ClientApp/src/components/NavList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './NavList.css';
+import {Marquee} from "./Marquee";
 
 export class NavList extends Component {
   static displayName = NavList.name;
@@ -7,6 +8,7 @@ export class NavList extends Component {
   render () {
     return (
       <header>
+        <Marquee text="rawr" />
         <ul>
           <li><a href="https://swungcat.coffeetariat.net/">Swungcat API</a></li>
           <li><a href="https://scauth0.coffeetariat.net/">Swungcat Auth API</a></li>
