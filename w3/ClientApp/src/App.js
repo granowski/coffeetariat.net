fc3b57f570089ec4cc0648d5121080905dccda77
Derrick Granowski <granowski@coffeetariat.net>
2019-08-05 15:15:45 -0400

    wip; preparing for coding against a backend service

--- w3/ClientApp/src/App.js
+++ w3/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import {IndexPage} from './components/IndexPage';
+import { UnpopularFeed } from "./components/UnpopularFeed";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -10,7 +11,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route path='/' component={IndexPage} />
-        <Route path='/' componetn={UnpopularFeed} />
+        <Route path='/' component={UnpopularFeed} />
       </Layout>
     );
   }
